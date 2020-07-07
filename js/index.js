1d9c832ddb4c61fbc058916356aaa55e8b3c5239
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-07 15:42:08 -0400

    Added mouseover event to change colors of nav links to red

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// const launchButton = document.querySelector('#launchButton')
+
+// 1
+let navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(item => {
+    item.addEventListener('mouseover', event => {
+        item.style.color = 'red';
+    })
+})
+
+// 2
