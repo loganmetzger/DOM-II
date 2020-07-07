028c0ea1020499d89d32d68ed448096d9de0a810
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-07 15:48:25 -0400

    create mouseleave event for nav items to turn them blue

--- js/index.js
+++ js/index.js
@@ -4,9 +4,16 @@
 // 1
 let navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(item => {
-    item.addEventListener('mouseover', event => {
+    item.addEventListener('mouseenter', event => {
         item.style.color = 'red';
     })
 })
 
 // 2
+navLinks.forEach(item => {
+    item.addEventListener('mouseleave',event  =>{
+        item.style.color = 'dodgerblue';
+    })
+})
+
+// 3
