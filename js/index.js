2bc11ffdecf87d6b0b6b51a882b26b2ee4bced0a
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-07 16:22:41 -0400

    trouble with changing paragraphs using for each for 4, should have prevent default done correctly

--- js/index.js
+++ js/index.js
@@ -11,9 +11,36 @@ navLinks.forEach(item => {
 
 // 2
 navLinks.forEach(item => {
-    item.addEventListener('mouseleave',event  =>{
+    item.addEventListener('mouseleave', event  =>{
         item.style.color = 'dodgerblue';
     })
 })
 
 // 3
+document.body.addEventListener('keydown', event => {
+    if (event.key === 'q') {
+        document.body.remove();
+    }
+})
+
+// 4
+// e.target.style.visibility = 'hidden';
+
+let paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+paragraphs.forEach(item => {
+    item.addEventListener('onselect', event => {
+        item.target.style.color = 'dodgerblue';
+    })
+})
+
+// 5
+
+
+
+// prevent default 
+navLinks.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+})
